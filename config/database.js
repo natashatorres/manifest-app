055f983070a94f5931af247e8e1c96093cea5ff8
Natasha Torres <natashaxtorres@gmail.com>
2021-10-26 16:45:39 -0400

    removed config from gitignore

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://yoda:babyyoda@cluster0.ca0uj.mongodb.net/seeds?retryWrites=true&w=majority', 
+    'dbName': 'seeds'
+};
+
+//this will export an object
